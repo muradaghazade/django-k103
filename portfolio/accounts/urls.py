9969a401012957533825cd07ba1055525422d77a
muradaghazade <xomurad41@gmail.com>
2025-01-09 16:31:51 +0400

    register added

--- portfolio/accounts/urls.py
+++ portfolio/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from accounts.views import register
 
 app_name = 'accounts'
 
 urlpatterns = [
-    
+    path('register/', register, name='register'),
 ]
--- portfolio/accounts/views.py
+++ portfolio/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from accounts.forms import RegisterForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'index.html')
+    form = RegisterForm()
+    return render(request, 'register.html', {'form':form})
